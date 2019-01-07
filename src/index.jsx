cca7974efc2acb4cf6f09bf4b62bcd6797c90da5
freya0608 <freya001@yeah.net>
2019-01-07 11:08:37 +0800

    init

---
+++ src/index.jsx
@@ -0,0 +1,1 @@
+import {observable} from 'mobx';
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const config={
+
+    mode:'development',
+    entry:path.resolve(__dirname,'src/index.jsx'),
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'main.js'
+    },
+    module:{
+        rules:[{
+            test:/\.jsx?$/,
+            exclude:/node_modules/,
+            use:{
+                loader:'babel-loader',
+                options:{
+                    presets:['env'],
+                    plugins:['transform-decorators-legacy']
+                }
+            }
+        }]
+    },
+    devtool:'inline-source-map'
+};
